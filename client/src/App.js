39094c1416689b7de7768d3be2a0858ca013c455
Damla Bortucen <damla.bortucen.24@ucl.ac.uk>
2025-06-22 13:28:46 +0200

    built the input todo component on the client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
+//components
+import InputTodo from "./components/InputTodo"
+
+
 function App() {
-  return <></>;
+  return <>
+    <div className='container'>
+      <InputTodo />
+    </div>
+  </>;
 }
 
 export default App;
